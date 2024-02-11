afbd31dd6d2bfbac03b22241dacf677d90a3d7d7
PetroBoi841! <pertoblitz.841@gmail.com>
2024-02-11 22:08:58 +0200

    run 4

--- robot_runs.py
+++ robot_runs.py
@@ -2,10 +2,10 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
+from pybricks.tools import wait, StopWatch, hub_menu
 
 class Gearbox:
-    def __init__(self, shifter: Motor, output: Motor, gears = 4, gear_distance=1470):
+    def __init__(self, shifter: Motor, output: Motor, gears = 4, gear_distance=1465):
         """Creates a gear shift using 2 motors."""
         self.shifter = shifter
         self.output = output
@@ -13,11 +13,11 @@ class Gearbox:
         self.gear_distance = gear_distance
 
         self.shift_speed = 720
-        self.stall_threshold = 110
+        self.stall_threshold = 105
 
     def reset(self):
         self.shifter.dc(-90)
-        while self.shifter.load() < self.stall_threshold:
+        while self.shifter.load() < self.stall_threshold or self.shifter.angle() > 100:
             pass
         self.shifter.hold()
         self.shifter.run_angle(self.shift_speed, 540)
@@ -36,31 +36,53 @@ class Gearbox:
 hub = PrimeHub()
 hub.imu.reset_heading(0)
 
-changeM = Motor(Port.A) #blue
-outputM = Motor(Port.C) #purple
+changeM = Motor(Port.B) #blue
+outputM = Motor(Port.D) #purple
 gear_box = Gearbox(changeM, outputM)
 
-left_wheel = Motor(Port.B) #green
-right_wheel = Motor(Port.E, positive_direction=Direction.COUNTERCLOCKWISE) #aqua
+left_wheel = Motor(Port.E, positive_direction=Direction.COUNTERCLOCKWISE) #green
+right_wheel = Motor(Port.F) #aqua
 wheels = DriveBase(left_wheel, right_wheel, 62, 129)
-# wheels.use_gyro(True)
+wheels.use_gyro(True)
 
-right_sensor = ColorSensor(Port.F) #yellow
-left_sensor = ColorSensor(Port.D) #red
+right_sensor = ColorSensor(Port.A) #yellow
+left_sensor = ColorSensor(Port.C) #red
+
+#================================RUNS==================================
+
+def run_4():
+    wheels.settings(straight_speed=300)
+    wheels.straight(-850)
+    wheels.straight(1050)
+
+
+run_4()
+#================================TESTS=================================
+
+# gear_box.reset()
+# while Button.LEFT not in hub.buttons.pressed():
+#     gear = hub_menu("1", "2", "3", "4") if hub != 3 else 2.5
+#     gear_box.shift_to(int(gear))
+#     gear_box.output.dc(100)
+#     while Button.RIGHT not in hub.buttons.pressed():
+#         pass
+#     gear_box.output.hold()
+#     gear_box.reset()
+
 
 # wheels.settings(straight_speed=200)
 # wheels.straight(30000)
 
-gear_box.reset()
-gear_box.output.run_time(730, 5000)
-gear_box.shift_to(4)
-gear_box.output.run_time(730, 5000)
-gear_box.shift_to(2)
-gear_box.output.run_time(730, 5000)
-gear_box.reset()
-gear_box.output.run_time(730, 5000)
-gear_box.shift_to(3)
-gear_box.output.run_time(730, 5000)
+# gear_box.reset()
+# gear_box.output.run_time(730, 5000)
+# gear_box.shift_to(4)
+# gear_box.output.run_time(730, 5000)
+# gear_box.shift_to(2)
+# gear_box.output.run_time(730, 5000)
+# gear_box.reset()
+# gear_box.output.run_time(730, 5000)
+# gear_box.shift_to(3)
+# gear_box.output.run_time(730, 5000)
 
 # right_wheel.run_time(500, 1000)
 # left_wheel.run_time(500, 1000)
